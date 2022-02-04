fe5cede3a971fded66e6be9765a8fb1bbb2db423
mosharaf <mosharaf@pixmama.com>
2022-02-04 17:34:20 +0600

    remove unnecessary pages create final version of portfolio page

--- pages/index.vue
+++ pages/index.vue
@@ -2,40 +2,62 @@
   <div class="flex items-center">
     <div class="w-490px flex-grow-0">
       <div
-        id="photo"
-        class="bg-center bg-white bg-cover bg-no-repeat pb-150 shadow"
+          id="photo"
+          class="bg-center bg-white bg-cover bg-no-repeat pb-150 shadow"
       ></div>
     </div>
     <div v-mouse-move class="flex-1 pl-8 pt-16">
       <h1
-        class="text-black uppercase font-extrabold text-4xl leading-none mb-5"
+          class="text-black uppercase font-extrabold text-4xl leading-none mb-5"
       >
         {{ firstName }}
         <div class="relative">
           <div
-            class="absolute w-120px right-100 top-50 border-blue border-b-5 line-transform"
+              class="absolute w-120px right-100 top-50 border-blue border-b-5 line-transform"
           ></div>
           <span class="text-blue">{{ lastName }}</span>
         </div>
       </h1>
       <h2 class="mb-2">{{ position }}</h2>
-      <p class="text-gray">
-        {{ aboutMe }}
-      </p>
+      <div class="w-4/5">
+        <p class="text-black text-sm">
+          {{ aboutMe }}
+        </p>
+      </div>
+      <div class="mt-4">
+        <div class="mb-4">
+          <div class="text-lg mb-4">Academic & Professional Interests</div>
+          <span v-for="interest in academicInterest" class="bg-green-700 text-white p-2 m-2 rounded-bl-xl">
+            {{ interest }}
+          </span>
+        </div>
+
+        <div>
+          <div class="text-lg mb-4">Other Interests</div>
+          <span v-for="interest in otherInterest" class="bg-green-700 text-white p-2 m-2 rounded-bl-xl">
+            {{ interest }}
+          </span>
+        </div>
+      </div>
+
+
+      <div class="cv-button mt-16">
+          <span class="flex ">
+             <a class="uppercase text-sm  text-white bg-blue text-left font-bold px-4 py-2 block hover:border"
+                href="/Md%20Mosharaf%20Hossan.pdf">Download my CV</a>
+          </span>
+
+      </div>
+
+
     </div>
   </div>
 </template>
 
 <script>
-import { getAge } from '~/utils/functions'
-import { mouseMove } from '~/utils/directives'
-import {
-  firstName,
-  lastName,
-  position,
-  birthday,
-  aboutMe
-} from '~/docs/profile.json'
+import {getAge} from '~/utils/functions'
+import {mouseMove} from '~/utils/directives'
+import {aboutMe, birthday, firstName, lastName, position} from '~/docs/profile.json'
 
 export default {
   directives: {
@@ -47,7 +69,10 @@ export default {
       firstName,
       lastName,
       position,
-      aboutMe
+      aboutMe,
+      academicInterest: ['Distributed Systems', 'Computer Vision', 'Artificial Intelligence'],
+      otherInterest: ['Psychology', 'Bioinformatics', 'Badminton', 'Table Tennis', 'Formula 1'],
+      publicPath: process.env.BASE_URL
     }
   },
 
@@ -63,6 +88,11 @@ export default {
 .line-transform {
   transform: translate(-5px, -50%);
 }
+
+.interest {
+  background: #14A26B;
+}
+
 #photo {
   background-image: url('~assets/images/mosharaf3.jpeg');
 }
