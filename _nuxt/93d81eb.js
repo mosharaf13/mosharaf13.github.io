0f87fe63f4c3c3c74c512ee912ff758343d0e7cc
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2025-04-14 13:06:40 -0600

    65da1ac

---
+++ _nuxt/93d81eb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{196:function(n,e,t){"use strict";t.r(e);var l=t(20),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n  Resume\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
